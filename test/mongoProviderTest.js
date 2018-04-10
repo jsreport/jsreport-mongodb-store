81a2a1f1de54ad7f528a53ff552f69e8e7336b60
pofider <jan.blaha@hotmail.com>
2018-04-10 19:20:46 +0200

    expose out from provider only js types, hide ObjectID and Binary

--- test/mongoProviderTest.js
+++ test/mongoProviderTest.js
@@ -1,4 +1,4 @@
-require('should')
+const should = require('should')
 const jsreport = require('jsreport-core')
 
 describe('mongodb store', () => {
@@ -8,10 +8,47 @@ describe('mongodb store', () => {
     reporter = jsreport({ store: { provider: 'mongodb' } })
     reporter.use(require('../')({ 'address': '127.0.0.1', 'port': 27017, 'databaseName': 'test' }))
 
+    reporter.use(() => {
+      reporter.documentStore.registerEntityType('TestType', {
+        _id: { type: 'Edm.String', key: true },
+        name: { type: 'Edm.String', key: true, publicKey: true },
+        content: { type: 'Edm.Binary', document: { extension: 'html', content: true } }
+      })
+      reporter.documentStore.registerEntitySet('test', { entityType: 'jsreport.TestType' })
+    })
     await reporter.init()
   })
 
   afterEach(() => reporter.close())
+
+  it('should return true node buffers', async () => {
+    await reporter.documentStore.collection('test').insert({
+      name: 'foo',
+      content: Buffer.from('foo')
+    })
+
+    const doc = await reporter.documentStore.collection('test').findOne({ name: 'foo' })
+    Buffer.isBuffer(doc.content).should.be.true()
+  })
+
+  it('should return string instead of ObjectId from insert', async () => {
+    const doc = await reporter.documentStore.collection('test').insert({
+      name: 'foo',
+      content: Buffer.from('foo')
+    })
+
+    doc._id.should.have.type('string')
+  })
+
+  it('should remove entity by _id', async () => {
+    const doc = await reporter.documentStore.collection('test').insert({
+      name: 'foo'
+    })
+
+    await reporter.documentStore.collection('test').remove({ _id: doc._id })
+    const loadedDoc = await reporter.documentStore.collection('test').findOne({ name: 'foo' })
+    should(loadedDoc).not.be.ok()
+  })
 
   jsreport.tests.documentStore()(() => reporter.documentStore)
 })
